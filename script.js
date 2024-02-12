13ad9c1919a0ebb1aad10a12cd5482313ea1f372
RicardoSTU <147540745+RicardoSTU@users.noreply.github.com>
2024-02-12 17:40:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function login() {
+    // Assuming a successful login for demonstration purposes
+    // You may need to add actual authentication logic here
+
+    // Redirect to the specified URL
+    window.location.href = 'https://ricardostu.github.io/Repqid/';
+
+    // Prevent the form from submitting (since we're handling the redirection via JavaScript)
+    return false;
+}
